76ed08d97a8f7c1382c48f0406c5d0a400eb47bc
Nikita Antonov <nikita45454@gmail.com>
2025-06-15 14:45:27 +0500

    feat: добавляет адаптив для секции Partners

--- src/components/Sections/Partners.vue
+++ src/components/Sections/Partners.vue
@@ -2,32 +2,33 @@
   <section id="partners">
     <div class="wrapper">
       <h2 class="h2 text-center title">Партнеры</h2>
-      <Swiper
-        slides-per-view="auto"
-        space-between="120"
-      >
-        <SwiperSlide class="slide">
-          <img width="100" height="100" src="@/assets/images/partners/fitness-team.svg" loading="lazy" alt="FITNESS TEAM">
-        </SwiperSlide>
-        <SwiperSlide class="slide">
-          <img width="119" height="124" src="@/assets/images/partners/barbara.png" loading="lazy" alt="Barbara Fitness">
-        </SwiperSlide>
-        <SwiperSlide class="slide">
-          <img width="175" height="82" src="@/assets/images/partners/mendeleef.svg" loading="lazy" alt="Mendeleef Fitness">
-        </SwiperSlide>
-        <SwiperSlide class="slide">
-          <img width="216" height="105" src="@/assets/images/partners/zorge.svg" loading="lazy" alt="Zorge Fitness">
-        </SwiperSlide>
-        <SwiperSlide class="slide">
-          <img width="155" height="92" src="@/assets/images/partners/korolef.svg" loading="lazy" alt="Korolef Fitness">
-        </SwiperSlide>
-      </Swiper>
+      <div class="items">
+        <div class="row1">
+          <div class="item">
+            <img class="item-img" width="100" height="100" src="@/assets/images/partners/fitness-team.svg" loading="lazy" alt="FITNESS TEAM">
+          </div>
+          <div class="item">
+            <img class="item-img" width="119" height="124" src="@/assets/images/partners/barbara.png" loading="lazy" alt="Barbara Fitness">
+          </div>
+          <div class="item">
+            <img class="item-img" width="175" height="82" src="@/assets/images/partners/mendeleef.svg" loading="lazy" alt="Mendeleef Fitness">
+          </div>
+        </div>
+        <div class="row2">
+          <div class="item">
+            <img class="item-img" width="216" height="105" src="@/assets/images/partners/zorge.svg" loading="lazy" alt="Zorge Fitness">
+          </div>
+          <div class="item">
+            <img class="item-img" width="155" height="92" src="@/assets/images/partners/korolef.svg" loading="lazy" alt="Korolef Fitness">
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script setup>
-  import { Swiper, SwiperSlide } from 'swiper/vue';
+
 </script>
 
 <style scoped lang="scss">
@@ -35,11 +36,27 @@
     margin-bottom: 55px;
   }
 
-  .slide {
-    user-select: none;
-    width: fit-content;
+  .items {
     display: flex;
-    align-items: center;
-    height: auto !important;
+    justify-content: center;
+    column-gap: 120px;
+
+    @include lg {
+      flex-wrap: wrap;
+      row-gap: 35px;
+    }
+
+    @include sm {
+      column-gap: 48px;
+    }
+  }
+
+  .row1, .row2 {
+    display: flex;
+    column-gap: 120px;
+
+    @include sm {
+      column-gap: 48px;
+    }
   }
 </style>
